4f74c8c763085a03e5bb67b2f7b0e6d0bb1a87c3
Leedasol2 <dasol2267@naver.com>
2022-04-29 15:50:31 +0900

    [이다솔] reviewdao 생성+조회메서드작성

---
+++ SemiProject/src/main/java/dao/ReviewDao.java
@@ -0,0 +1,59 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.ReviewDto;
+import mysql.db.DbConnect;
+
+public class ReviewDao {
+
+	DbConnect db=new DbConnect();
+	
+	//[이다솔]로그인 중인 회원의 리뷰목록 조회
+	public List<ReviewDto> getSignupLesson(String mnum) {
+		
+		List<ReviewDto> list=new ArrayList<ReviewDto>();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement psmt=null;
+		ResultSet rs=null;
+		
+		String sql="select r.rnum, r.lnum, r.rstar, r.rcontents, r.rday from lesson l,review r where r.mnum=? and r.lnum=l.lnum";
+		
+		try {
+			psmt=conn.prepareStatement(sql);
+			
+			//바인딩
+			psmt.setString(1, mnum);
+			
+			rs=psmt.executeQuery();
+			
+			while(rs.next()) {
+				ReviewDto dto=new ReviewDto();
+				
+				dto.setRnum(rs.getString("rnum"));
+				dto.setLnum(rs.getString("lnum"));
+				dto.setRstar(rs.getInt("rstar"));
+				dto.setRcontents(rs.getString("lnum"));
+				dto.setRday(rs.getTimestamp("rday"));
+				
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, psmt, conn);
+		}
+		
+		return list;
+	}
+	
+	
+	
+}
